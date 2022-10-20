c049518eaebd0d5d309544930702cc8ec387d4c2
Pedro Cristo <pcristo35@gmail.com>
2022-10-20 14:00:50 +0000

    Add context processor to display categories globaly

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.products,  name='products/'),
+    path('', views.products,  name='products'),
     path('<slug:category_slug>/',
          views.products,  name='products_by_category'),
 ]
