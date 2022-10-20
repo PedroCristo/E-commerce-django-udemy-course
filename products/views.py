be9474b6063301cfad71f3543430d723d7778460
Pedro Cristo <pcristo35@gmail.com>
2022-10-20 13:15:50 +0000

    Add statement and url to sort products by category

--- products/views.py
+++ products/views.py
@@ -1,16 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
+from category.models import Category
 
 
-def products(request):
+def products(request, category_slug=None):
     """
     View to render the products page
     """
-    products = Product.objects.all().filter(is_available=True)
-    products_count = products.count()
+    categories = None
+    products = None
+
+    if category_slug is not None:
+        categories = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(
+            category=categories, is_available=True)
+        products_count = products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        products_count = products.count()
+
     context = {
         'products': products,
         'products_count': products_count,
     }
     return render(request, 'products/products.html', context)
-
