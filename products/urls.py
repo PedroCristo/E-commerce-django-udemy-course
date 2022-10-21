935a49914713f39f0a6a3111ab0c5885135cf3c0
Pedro Cristo <pcristo35@gmail.com>
2022-10-21 11:08:40 +0000

    Add views, URL pathe and template for product detail

--- products/urls.py
+++ products/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.products,  name='products'),
     path('<slug:category_slug>/',
          views.products,  name='products_by_category'),
+    path('<slug:category_slug>/<slug:product_slug>',
+         views.product_detail,  name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -24,3 +24,18 @@ def products(request, category_slug=None):
         'products_count': products_count,
     }
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, category_slug, product_slug):
+    """
+    View to render the product detail page
+    """
+    try:
+        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e:
+        raise e 
+
+    context = {
+        'single_product': single_product,
+    }        
+    return render(request, 'products/product_detail.html', context)    
