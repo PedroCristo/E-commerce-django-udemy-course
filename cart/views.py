318ed82ede3c93110718bd5a3b10e6c94bf1e1d2
Pedro Cristo <pcristo35@gmail.com>
2022-10-24 16:37:47 +0000

    Fix issues for cart view

--- cart/views.py
+++ cart/views.py
@@ -68,12 +68,14 @@ def cart(request, total=0, quantity=0, cart_items=None):
     View to render the cart page
     """
     try:
+        tax = 0
+        gran_total = 0
         cart = Cart.objects.get(cart_id=_cart_id(request))
         cart_items = CartItem.objects.filter(cart=cart, is_active=True)
         for cart_item in cart_items:
             total += (cart_item.product.price * cart_item.quantity)
             quantity += (cart_item.quantity)
-        tax = (2 * total)/100
+        tax = (22 * total)/100
         grand_total = total + tax
     except ObjectDoesNotExist:
         pass
--- products/views.py
+++ products/views.py
@@ -19,7 +19,8 @@ def products(request, category_slug=None):
             category=categories, is_available=True)
         products_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available=True)
+        products = Product.objects.all().filter(
+            is_available=True).order_by('id')
         products_count = products.count()
     paginator = Paginator(products, 6)
     page = request.GET.get('page')
