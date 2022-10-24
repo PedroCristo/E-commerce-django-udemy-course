7dbb11757389fb13477379e33c57b31ee76f42fe
Pedro Cristo <pcristo35@gmail.com>
2022-10-24 14:24:47 +0000

    Add filter and statement to show product already added to the cart

--- products/views.py
+++ products/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404,  redirect
 from .models import Product
 from category.models import Category
+from cart.models import CartItem
+from cart.views import _cart_id
 
 
 def products(request, category_slug=None):
@@ -33,10 +35,12 @@ def product_detail(request, category_slug, product_slug):
     try:
         single_product = Product.objects.get(
             category__slug=category_slug, slug=product_slug)
+        in_cart = CartItem.objects.filter(cart__cart_id=_cart_id(request), product=single_product).exists()
     except Exception as e:
         raise e
 
     context = {
         'single_product': single_product,
+        'in_cart': in_cart,
     }
     return render(request, 'products/product_detail.html', context)
